98df41a3bef28cd7453e8f3cf2b35e4ac17340d8
Ryan Buxton <grmaverick95@gmail.com>
2020-10-20 01:06:30 +0100

    CL#48: Perpetual Upgrade Job

--- Screeps/arbiter.controller.js
+++ Screeps/arbiter.controller.js
@@ -11,10 +11,8 @@ function Log(_string){
 module.exports.NotifyDeath = function(_creepName) {
 	let found = Memory.ControllerJobs.find(element => element.Assignee == _creepName);
 	if(found != null) {
-		found.Assigned = false;
-		found.Assignee = null;
-		Log("Notification of Death of " + _creepName + ": Reposting old job!");
-		PostCreatedJob(found.Id, found.Type);
+		found.Assignees.splice(found.Assignees.indexOf(_creepName), 1);
+		Log("Notification of Death of " + _creepName + ": Room for one more!");
 	}
 }
 
@@ -38,39 +36,31 @@ module.exports.OnPerpetualJobAssigned = function(_callback) {
 }
 
 module.exports.AssignCreepToJob = function(_creep, _jobId) {
+
 	let found = Memory.ControllerJobs.find(element => element.Id == _jobId);
 	if(found != null) {
-		if(found.Assigned) {
-			Log("Job is already assigned: " + _jobId);
-		}
-		else{
-			found.Assigned = true;
-			found.Assignee = _creep.name;
-			_creep.memory.job = found;
-			Log("Resource Job Assigned: "+found.Assignee+" "+found.Id);
-			PostJobAssigned(_creep, found.Id, found.Type);
-		}
+		_creep.memory.job = found;
+		found.Assignees.push(_creep.name);
+		Log("Resource Job Assigned: "+found.Assignee+" "+found.Id);
+		PostPerptualJobAssigned(_creep, found.Id, found.Type);
 	}
 	else{
-		Log("Job does not exist: "+_jobId);
+		Log("[ResourceArbiter]: Job does not exist: "+_jobId);
 	}
 }
 
-module.exports.Update = function()
-{
-	var closestResourceId = Game.spawns["Spawn1"].room.controller.pos.findClosestByPath(FIND_SOURCES).id;
-	for(let idx = 0; idx < 3; idx++) {
-		let found = Memory.ControllerJobs.find(element => element.Id == "Upgrade"+"_"+idx);
-		if(found == null) {
-			var job = {
-				Id:"Upgrade"+"_"+idx,
-				Type:"Upgrader",
-				Assigned: false,
-				ControllerId: Game.spawns["Spawn1"].room.controller.id,
-				ResourcePoints: Utilities.GetResourcePoints(Game.spawns["Spawn1"].room),
-			};
-			Memory.ControllerJobs.push(job);
-			PostCreatedJob(job.Id, job.Type);
-		}
+module.exports.Update = function() {
+	// Perpetual Job
+	let found = Memory.ControllerJobs.find(element => element.Id == "Upgrading");
+	if(found == null) {
+		var job = {
+			Id:"Upgrading",
+			Type:"Upgrader",
+			ControllerId: Game.spawns["Spawn1"].room.controller.id,
+			Assignees: []
+		};
+
+		Memory.ControllerJobs.push(job);
+		PostCreatedJob(job.Id, job.Type);
 	}
 }
