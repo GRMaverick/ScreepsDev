4973e55358be82e5be58c664f47472c0f40f7a0d
Ryan Buxton <grmaverick95@gmail.com>
2020-10-20 01:35:56 +0100

    CL#50: Harvesters can deliver to Storage and Containers now

--- Screeps/role.service.actions.js
+++ Screeps/role.service.actions.js
@@ -31,7 +31,12 @@ module.exports.Deliver = function(_creep, _data) {
 
 	let resourceTargets = _creep.room.find(FIND_STRUCTURES, {
 		filter: (structure) => {
-			return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_TOWER) &&
+			return (
+					structure.structureType == STRUCTURE_EXTENSION ||
+					structure.structureType == STRUCTURE_SPAWN ||
+					structure.structureType == STRUCTURE_CONTAINER ||
+					structure.structureType == STRUCTURE_STORAGE ||
+					structure.structureType == STRUCTURE_TOWER) &&
 			   structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
 		}
 	});
